e3f7d1e078f753d6feaf3a8a496476a3baac27d1
RodrigoBoehme <robi.boehme@gmail.com>
2026-03-05 19:55:39 -0300

    Miscelaneous changes to ./games/fnaf.js

--- games/fnaf.js
+++ games/fnaf.js
@@ -1,15 +1,63 @@
 const readline = require("readline-sync");
 const gameState = require("../gameState");
 
+function checkLifes(){
+  if(gameState.getLives==0){return true}else{return false}
+}
 function fnafAlikeGame() {
+  while (true) {
+    console.log(
+      "What night you wanna play? \n1) First Night\n2) Second Nigth\n3) Third Night \n0) Exit Game",
+    );
+    let option = readline.question("Input number: ");
+    switch (option) {
+      case "1":
+        fnafAlike(3,5,2,1)
+        if(checkLifes()){return}
+        break;
+      case "2":
+        fnafAlike(5, 7, 3, 4);
+        if(checkLifes()){return}
+      break;
+      case "3":
+        fnafAlike(1, 9, 8, 7);
+        if(checkLifes()){return}
+      break;
+      case "4":
+        console.log("Custom Night, Set the difficulty for each animatronic: (Min: 0, Max: 10)")
+        let bn=readline.questionInt("Bonnie: ")
+        let fr=readline.questionInt("Freddy: ")
+        let fx=readline.questionInt("Foxy: ")
+        let ch=readline.questionInt("Chica: ")
+        fnafAlike(bn,fr,fx,ch)
+        if(checkLifes()){return}
+
+      break;
+
+      case "0":return;
+
+      case "1987":
+        readline.question("This is an easter egg, but you still lose tho, press enter to continue")
+        gameState.loseLife();
+      return;
+      default:
+        readline.question("Invalid input, press ENTER to continue...")
+      break;
+      
+        
+    }
+  }
+}
+
+function fnafAlike(Bd, Fd, Fx, Cd) {
   let r = 0;
   let energy = 30;
   let door = true;
   let animatronics = [
-    { name: "Bonnie", position: 0, path: 6, level: 5 ,canAtk:false},
-    { name: "Freddy", position: 0, path: 8, level: 7 ,canAtk:false},
-    { name: "Foxy", position: 0, path: 5, level: 3 ,canAtk:false},
-    { name: "Chica", position: 0, path: 6, level: 4 ,canAtk:false},
+    { name: "Bonnie", position: 0, path: 6, level: Bd, canAtk: false },
+    { name: "Freddy", position: 0, path: 8, level: Fd, canAtk: false },
+    { name: "Foxy", position: 0, path: 5, level: Fx, canAtk: false },
+    { name: "Chica", position: 0, path: 6, level: Cd, canAtk: false },
   ];
 
   console.clear();
@@ -72,26 +120,35 @@ function fnafAlikeGame() {
     }
 
     for (let aa = 0; aa < animatronics.length; aa++) {
-      if (animatronics[aa].position >= animatronics[aa].path && door&&animatronics[aa].canAtk) {
+      if (
+        animatronics[aa].position >= animatronics[aa].path &&
+        door &&
+        animatronics[aa].canAtk
+      ) {
         if (Math.floor(Math.random() * 10 + 1) <= animatronics[aa].level) {
           readline.question("You got jumpscare by " + animatronics[aa].name);
           gameState.loseLife();
           return;
         }
-      } else if (animatronics[aa].position >= animatronics[aa].path&&animatronics[aa].canAtk) {
+      } else if (
+        animatronics[aa].position >= animatronics[aa].path &&
+        animatronics[aa].canAtk
+      ) {
         console.log(
           animatronics[aa].name + " was at the door, but it went back!",
         );
         animatronics[aa].position = 1;
-        animatronics[aa].canAtk=false
-      } else if(animatronics[aa].position >= animatronics[aa].path&&!animatronics[aa].canAtk){
-          animatronics[aa].canAtk=true
-
-      } 
-      
-      else {
+        animatronics[aa].canAtk = false;
+      } else if (
+        animatronics[aa].position >= animatronics[aa].path &&
+        !animatronics[aa].canAtk
+      ) {
+        animatronics[aa].canAtk = true;
+      } else {
         if (Math.floor(Math.random() * 10 + 1) <= animatronics[aa].level) {
-          if(Math.floor(Math.random()*20+1)<=2){animatronics[aa].position++}
+          if (Math.floor(Math.random() * 20 + 1) <= 2) {
+            animatronics[aa].position++;
+          }
           animatronics[aa].position++;
         }
       }
